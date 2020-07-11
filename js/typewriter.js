4127a31404353edf815f712061e39d424b273a12
Saakshaat <saakshaat2001@gmail.com>
2020-07-10 17:35:51 -0700

    Updating typerwriter speed

--- js/typewriter.js
+++ js/typewriter.js
@@ -21,7 +21,7 @@ TxtRotate.prototype.tick = function() {
   this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
   var that = this;
-  var delta = 100 - Math.random() * 100;
+  var delta = 70 - Math.random() * 100;
 
   if (this.isDeleting) { delta /= 2; }
 
@@ -31,7 +31,7 @@ TxtRotate.prototype.tick = function() {
   } else if (this.isDeleting && this.txt === '') {
     this.isDeleting = false;
     this.loopNum++;
-    delta = 500;
+    delta = 50;
   }
 
   setTimeout(function() {
