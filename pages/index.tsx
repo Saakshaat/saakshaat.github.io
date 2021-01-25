6b271490ef146303bbfcad31708ae42ca1abd51e
Saakshaat <saakshaat2001@gmail.com>
2021-01-25 17:36:11 +0530

    Remove header from Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,17 +44,6 @@ export default function Home() {
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
