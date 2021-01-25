830c012889f6545adf89324ff46c9f7faf6458cc
Saakshaat <saakshaat2001@gmail.com>
2021-01-25 17:17:03 +0530

    Remove commented codeblock

--- components/layout.tsx
+++ components/layout.tsx
@@ -34,20 +34,3 @@ export class GlobalLayout extends Component {
   }
 }
 
-/*
-    <div>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Saakshaat's silly mind dump." />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=dark&md=1&fontSize=175px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <div>{children}</div>
-    </div>
-*/
