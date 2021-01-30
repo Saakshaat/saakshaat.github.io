80bce95651367beae8dda2583c490715117650a9
Saakshaat <saakshaat2001@gmail.com>
2021-01-30 20:21:39 +0530

    Minor reformat

--- components/layouts.js
+++ components/layouts.js
@@ -3,22 +3,25 @@ import Head from "next/head";
 const name = "Saakshaat Singh";
 export const siteTitle = "**Saakshaat** Singh";
 
-export function GlobalLayout() {
+export function GlobalLayout({ children }) {
   return (
-    <Head>
-      <link rel="icon" href="/favicon.ico" />
-      <meta
-        name="description"
-        content="Saakshaat Singh. SWE. Chair. Baking Bacon Pancakes."
-      />
-      <meta
-        property="og:image"
-        content={`https://og-image.now.sh/${encodeURI(
-          siteTitle
-        )}.png?theme=dark&md=1&fontSize=140px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-      />
-      <meta name="og:title" content={name} />
-      <meta name="twitter:card" content="summary_large_image" />
-    </Head>
+    <div>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Saakshaat Singh. SWE. Chair. Baking Bacon Pancakes."
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.now.sh/${encodeURI(
+            siteTitle
+          )}.png?theme=dark&md=1&fontSize=140px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={name} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      {children}
+    </div>
   );
 }
