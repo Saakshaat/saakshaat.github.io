03bae6c9be40ffdd4f359c3af8991b00829097a3
Saakshaat <saakshaat2001@gmail.com>
2021-01-31 16:34:32 +0530

    Update Typewriter params with cursorColor

--- components/typewriter.js
+++ components/typewriter.js
@@ -1,11 +1,29 @@
 import ReactRotatingText from "react-rotating-text";
 
 import style from "./typewriter.module.scss";
+import utilStyles from "../styles/utils.module.scss";
 
-export default function Typerwriter({ elements }) {
+export default function Typerwriter({ elements, cursorColor }) {
   return (
     <div>
-      <ReactRotatingText items={elements} typingInterval={30} deletingInterval={30} pause={1000} cursor={false}/><text className={style.cursor}>▍</text>
+      <ReactRotatingText
+        items={elements}
+        typingInterval={30}
+        deletingInterval={30}
+        pause={1000}
+        cursor={false}
+      />
+      <text
+        className={`${style.cursor} ${
+          cursorColor === "white"
+            ? utilStyles.white
+            : cursorColor === "black"
+            ? utilStyles.black
+            : utilStyles.default
+        }`}
+      >
+        ▍
+      </text>
     </div>
   );
 }
