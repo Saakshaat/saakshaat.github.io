f5291f2f6b36a0e73790f5f3f407eb851a566318
Saakshaat <saakshaat2001@gmail.com>
2021-02-22 22:37:58 +0530

    Update animation styles for ProjectCards

--- components/cards/projectCards.js
+++ components/cards/projectCards.js
@@ -20,7 +20,7 @@ export function ProjectCardCollapsed({ name, image, tags, brief }) {
       rotate: [10, 20, 10, 0],
       opacity: ["0.2", "0.4", "0.8", "1"],
       backgroundSize: "100%",
-      boxShadow: "-0.5px 0.5px 5px 0.5px rgb(0, 0, 0)",
+      boxShadow: "-0.5px 0.5px 5px 0.5px rgb(90, 72, 72)",
       transition: {
         duration: "0.4",
       },
@@ -56,11 +56,11 @@ export function ProjectCardCollapsed({ name, image, tags, brief }) {
       }}
       whileHover={{
         backgroundSize: "120%",
-        boxShadow: `0px 0px 8px 3px rgb(0, 0, 0)`,
+        boxShadow: `0px 0px 8px 3px rgb(63, 51, 51)`,
       }}
       whileTap={{
         backgroundSize: "120%",
-        boxShadow: `0px 0px 8px 3px rgb(0, 0, 0)`,
+        boxShadow: `0px 0px 8px 3px rgb(63, 51, 51)`,
       }}
       transition={{
         duration: "0.2",
