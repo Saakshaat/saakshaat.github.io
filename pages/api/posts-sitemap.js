9b56022dba452c0516f4aeecd6b01f417df90347
Saakshaat <saakshaat2001@gmail.com>
2021-03-01 18:21:26 +0530

    Add sitemap through API

---
+++ pages/api/posts-sitemap.js
@@ -0,0 +1,39 @@
+import { SitemapStream, streamToPromise } from "sitemap";
+
+export default async (req, res) => {
+  try {
+    const smStream = new SitemapStream({
+      hostname: `https://${req.headers.host}`,
+      cacheTime: 600000,
+    });
+
+    // List of projects
+    const projects = [];
+
+    // Create each URL row
+    projects.forEach((project) => {
+      smStream.write({
+        url: `/project/${project.slug}`,
+        changefreq: "daily",
+        priority: 0.9,
+      });
+    });
+
+    // End sitemap stream
+    smStream.end();
+
+    // XML sitemap string
+    const sitemapOutput = (await streamToPromise(smStream)).toString();
+
+    // Change headers
+    res.writeHead(200, {
+      "Content-Type": "application/xml",
+    });
+
+    // Display output to user
+    res.end(sitemapOutput);
+  } catch (e) {
+    console.log(e);
+    res.send(JSON.stringify(e));
+  }
+};
